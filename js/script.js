66bb50429a1fc3b12b3ee169101c5dc6dce036fc
Joseph Lansang <josephlansang@gmail.com>
2018-01-08 21:36:51 +0800

    to do list

---
+++ js/script.js
@@ -0,0 +1,36 @@
+// alert("jQuery is working!");
+
+$("ul").on("click", "li", function() {
+<<<<<<< HEAD
+// $("li").click(function(){
+	// $(this).css("color", "lightgrey");
+// $("li").click(function() {
+	// $(this).css("color", "grey");
+
+	// $(this).css("text-decoration", "line-through");
+
+	$(this).toggleClass("completed");
+});
+
+$("ul").on("click", "span", function() {
+
+
+
+// $("span").click(function() {
+	$(this).parent().fadeOut(500, function() {
+		$(this).remove();
+	});
+	// event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event) {
+
+	// console.log(event.which);
+
+	if (event.which === 13) {  // 13 == enter key
+		var todoText = $(this).val();
+		$(this).val("");
+		$("ul").append("<li><span><i class='fa fa-trash-o' aria-hidden='true'></i></span> " + todoText + "</li>");
+	}
+
+});
